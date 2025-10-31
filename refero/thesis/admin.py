ab5a7fe6995d672904b95bb7cd58841708b72330
Netoerare <lawsmagnet6@gmail.com>
2025-10-31 22:18:12 +0800

    added models & requirements.txt

--- refero/thesis/admin.py
+++ refero/thesis/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import College, Program, Thesis, Tag
+
+@admin.register(College)
+@admin.register(Program)
+@admin.register(Thesis)
+@admin.register(Tag)
 
 # Register your models here.
