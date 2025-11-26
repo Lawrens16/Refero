423ea00fc6186d0bcec2bdaf78799bae011a1576
Netoerare <lawsmagnet6@gmail.com>
2025-11-26 12:45:42 +0800

    feat: Initialize API structure with models, views, and admin configuration; add Google and Facebook sign-in options, no functionality YET; navbar modified

--- refero/refero/settings.py
+++ refero/refero/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'refero',
     'widget_tweaks',
     'django.contrib.sites',
@@ -149,3 +150,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': [
+      'rest_framework.authentication.TokenAuthentication',
+  ],
+  'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.IsAuthenticated',
+  ],
+}
